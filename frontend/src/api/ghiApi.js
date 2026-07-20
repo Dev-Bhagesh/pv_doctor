0da9b9a8a417caf51dd8c343cab711f439fec6b8
Bhagesh <bhageshniloor159@gmail.com>
2026-07-20 15:18:25 +0530

    testing render URL

--- frontend/src/api/ghiApi.js
+++ frontend/src/api/ghiApi.js
@@ -2,5 +2,8 @@ const API_URL = import.meta.env.VITE_API_URL;
 export async function getGHIData() {
   const response = await fetch(`${API_URL}/data`);
   const data = await response.json();
+
+  console.log("API Response:", data);
+  console.log("Is Array?", Array.isArray(data));
   return data;
 }
